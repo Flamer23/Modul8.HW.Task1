d685233d7d657b2885106dd5983eba056e320f2a
Илья Зуев <zueviliya777@gmail.com>
2024-01-12 03:05:49 +0300

    Все задания

--- Modul8.HW.Task1/Program.cs
+++ Modul8.HW.Task1/Program.cs
@@ -4,7 +4,77 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "C:\\Users\\zuevi\\Desktop\\HW";
+
+            if (Directory.Exists(path))
+            {
+                Clear(path);
+            }
+            else
+            {
+                Console.WriteLine("Папка по указанному пути не существует");
+            }
+        }
+
+        public static void Clear(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            
+            DirectoryInfo[] subDirectories = directory.GetDirectories();
+            FileInfo[] files = directory.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                if (file.LastAccessTime.AddMinutes(30) < DateTime.Now) // не придумал как по другому проверить время
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch (FileNotFoundException ex)
+                    {
+                        Console.WriteLine("Файл не найден. Ошибка: " + ex.Message);
+                    }
+                    catch (UnauthorizedAccessException ex)
+                    {
+                        Console.WriteLine("Отсутствует доступ. Ошибка: " + ex.Message);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Произошла ошибка: " + ex.Message);
+                    }
+                    file.Delete(); 
+                }
+            }
+
+            foreach  (DirectoryInfo subDirectory in subDirectories)
+            {
+                if (subDirectory.LastAccessTime.AddMinutes(30) < DateTime.Now)
+                {
+                    try
+                    {
+                        subDirectory.Delete(true);
+                    }
+                    catch (DirectoryNotFoundException ex)
+                    {
+                        Console.WriteLine("Директория не найдена. Ошибка: " + ex.Message);
+                    }
+                    catch (UnauthorizedAccessException ex)
+                    {
+                        Console.WriteLine("Отсутствует доступ. Ошибка: " + ex.Message);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Произошла ошибка: " + ex.Message);
+                    }
+                    
+                }
+                else
+                {
+                    string subPath = subDirectory.FullName;
+                    Clear(subPath);
+                }
+            }
         }
     }
 }
